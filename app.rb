bc7a28f762c9900d05ce8af784e214326e6e3fac
jc <chengjoanne2@gmail.com>
2012-11-04 10:36:30 -0700

    default action

--- app.rb
+++ app.rb
@@ -5,6 +5,10 @@ require 'civic_info'
 account_sid = ENV['TWILIO_ACCT_SID']
 auth_token = ENV['TWILIO_AUTH_TOKEN']
 
+get '/' do
+  "hello world"
+end
+
 get '/sms' do
   twiml = Twilio::TwiML::Response.new do |r|
     r.Sms "testing testing 123"
